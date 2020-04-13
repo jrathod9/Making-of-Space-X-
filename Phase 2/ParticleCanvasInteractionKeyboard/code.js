7ed9ec58e291bc1bb60d50a20a355949b05731e5
Jay Rathod <lewisburnard@gmail.com>
2020-04-14 01:34:53 +0530

    Complete Phase 2

--- Phase 2/ParticleCanvasInteractionKeyboard/code.js
+++ Phase 2/ParticleCanvasInteractionKeyboard/code.js
@@ -3,7 +3,7 @@ var c = canvas.getContext('2d');
 canvas.width = window.innerWidth - 20;	
 canvas.height = window.innerHeight -20;	
 
-var keys = [];												//to track keys pressed 
+var keys = [];														//to track keys pressed 
 
 var particle = function(x,y,radius){
 	this.x = x;
@@ -11,22 +11,22 @@ var particle = function(x,y,radius){
 	this.radius = radius;	
 }
 
-var atom = new particle(100,100,30);							//Define atom particle 
+var atom = new particle(100,100,30);								
 var xspeed = 10;													//Define x direction speed  
-var yspeed = 7;
+var yspeed = 7;														//Define y direction speed
 function draw(){			
 	
-	c.clearRect(0,0,window.innerWidth,window.innerHeight);		//Clears the entire canvas 
+	c.clearRect(0,0,window.innerWidth,window.innerHeight);			//Clears the entire canvas 
 			
-	if(keys[37])
-		atom.x-=xspeed;
-	else if(keys[39])	
-		atom.x+=xspeed;
+	if(keys[37])				//if left is true
+		atom.x-=xspeed;			//move left by xspeed
+	else if(keys[39])			//else if right is true
+		atom.x+=xspeed;			//move right by xspeed
 
-	if(keys[38])
-		atom.y-=yspeed;
-	else if(keys[40])	
-		atom.y+=yspeed;
+	if(keys[38])				//if up is true
+		atom.y-=yspeed;			//move up by yspeed
+	else if(keys[40])			//else if down is true
+		atom.y+=yspeed;			//move down by yspeed
 
 	//Drawing the particle 
 	c.beginPath();
@@ -35,12 +35,12 @@ function draw(){
 	c.closePath();
 	c.fill();
 	
-	requestAnimationFrame(draw);								//Called inside the function
+	requestAnimationFrame(draw);								
 }
-
-draw();															//Initial function call
+draw();															
 			
 window.addEventListener("click",move,false);			
+
 window.addEventListener("keydown",keyPressed,false);	//keydown listener
 window.addEventListener("keyup",keyReleased,false);		//keyup listener
 
@@ -48,9 +48,10 @@ function move(e){
 	atom.x = e.x ;
 	atom.y = e.y; 
 }
-function keyPressed(e){				
+
+function keyPressed(e){				//sets value true when key pressed 
 	keys[e.keyCode] = true;
 }
-function keyReleased(e){
+function keyReleased(e){			//sets value false when key released
 	keys[e.keyCode] = false;
 }
